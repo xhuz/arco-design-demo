310abdc4a94b52a9c9df02b85cb59d2d086a8bb8
huzz <huzheng@wisdomgarden.com>
2023-11-27 11:56:28 +0800

    change public path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { vitePluginForArco } from '@arco-plugins/vite-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/dist',
   plugins: [vue(), vitePluginForArco()],
   css: {
     preprocessorOptions: {
